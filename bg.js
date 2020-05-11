9bad0840ea6dd5f77ad61d0ee3228e4620b95de1
haharimi <haharimi@naver.com>
2020-05-11 18:21:18 +0900

    image Background 작용 작업 완료

---
+++ bg.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 7;
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER); //숫자 0~2 까지에서 랜덤으로 숫자 배정 
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandom ();
+    paintImage(randomNumber);
+}
+
+init();
